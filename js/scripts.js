1bd25723d3b120a5871b73d0b239395dae9ce6cc
Kaxon <karenaxon@users.noreply.github.com>
2021-11-01 20:57:35 -0700

    Update files.

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,64 @@ $(document).ready(function(){
     $("#varNaming-defined").toggle();
   });
  
+  $("#function").click(function(){
+    $("#function-name").toggle();
+    $("#function-defined").toggle();
+  });
+
+  $("#methods").click(function(){
+    $("#methods-name").toggle();
+    $("#methods-defined").toggle();
+  });
+
+  $("#parameters").click(function(){
+    $("#parameters-name").toggle();
+    $("#parameters-defined").toggle();
+  });
+
+  $("#arguments").click(function(){
+    $("#arguments-name").toggle();
+    $("#arguments-defined").toggle();
+  });
+
+  $("#return").click(function(){
+    $("#return-name").toggle();
+    $("#return-defined").toggle();
+  });
+
+  $("#chaining").click(function(){
+    $("#chaining-name").toggle();
+    $("#chaining-defined").toggle();
+  });
+
+  $("#strings").click(function(){
+    $("#strings-name").toggle();
+    $("#strings-defined").toggle();
+  });
+
+  $("#booleans").click(function(){
+    $("#booleans-name").toggle();
+    $("#booleans-defined").toggle();
+  });
+
+  $("#undefined").click(function(){
+    $("#undefined-name").toggle();
+    $("#undefined-defined").toggle();
+  });
+
+  $("#nan").click(function(){
+    $("#nan-name").toggle();
+    $("#nan-defined").toggle();
+  });
+
+  $("#alert").click(function(){
+    $("#alert-name").toggle();
+    $("#alert-defined").toggle();
+  });
+
+  $("#concat").click(function(){
+    $("#concat-name").toggle();
+    $("#concat-defined").toggle();
+  });
+
 })
